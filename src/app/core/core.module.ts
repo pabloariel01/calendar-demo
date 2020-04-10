e4af1f3f15125dc0c995a3a41781d5fdd46ac2a6
pablo silva <pablo.silva@ey.com>
2020-04-10 20:16:06 -0300

    add store devtooks

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { NgxsModule } from '@ngxs/store';
+import { NgxsReduxDevtoolsPluginModule } from '@ngxs/devtools-plugin';
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule, NgxsModule.forRoot([]), NgxsReduxDevtoolsPluginModule.forRoot()]
 })
-export class CoreModule { }
+export class CoreModule {}
