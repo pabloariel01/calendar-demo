c10e8d1c30344807676734afe58409bc043667ba
pablo <pabloariel.silva@globant.com>
2020-04-11 14:52:26 -0300

    added storage persistance for events in app state

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,9 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NgxsModule } from '@ngxs/store';
 import { NgxsReduxDevtoolsPluginModule } from '@ngxs/devtools-plugin';
+import { NgxsStoragePluginModule } from '@ngxs/storage-plugin';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, NgxsModule.forRoot([]), NgxsReduxDevtoolsPluginModule.forRoot()]
+  imports: [
+    CommonModule,
+    NgxsModule.forRoot([]),
+    NgxsStoragePluginModule.forRoot(),
+    NgxsReduxDevtoolsPluginModule.forRoot()
+  ]
 })
 export class CoreModule {}
