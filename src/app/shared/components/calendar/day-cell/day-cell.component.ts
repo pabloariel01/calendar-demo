e64e2c7d31e103d4e5b14519ee078b7ef4c31e9f
pablo <pabloariel.silva@globant.com>
2020-04-11 02:16:27 -0300

    renamed component

---
+++ src/app/shared/components/calendar/day-cell/day-cell.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-day-cell',
+  templateUrl: './day-cell.component.html',
+  styleUrls: ['./day-cell.component.scss']
+})
+export class DayCellComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
