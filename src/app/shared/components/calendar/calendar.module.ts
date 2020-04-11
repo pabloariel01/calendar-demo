11c55e7ce53103ab06835256e92982d98ede71af
pablo <pabloariel.silva@globant.com>
2020-04-11 02:16:53 -0300

    renamed component

--- src/app/shared/components/calendar/calendar.module.ts
+++ src/app/shared/components/calendar/calendar.module.ts
@@ -2,12 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CalendarComponent } from './calendar.component';
 import { DateGridComponent } from './date-grid/date-grid.component';
-import { DayComponent } from './day/day.component';
 import { EventComponent } from './event/event.component';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { DayCellComponent } from './day-cell/day-cell.component';
 
 @NgModule({
-  declarations: [CalendarComponent, DateGridComponent, DayComponent, EventComponent],
+  declarations: [CalendarComponent, DateGridComponent, EventComponent, DayCellComponent],
   imports: [CommonModule, ReactiveFormsModule, FormsModule],
   exports: [CalendarComponent]
 })
