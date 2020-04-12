dc2db91a83b4a73ead0b1481e1a4086609c3a164
pablo <pabloariel.silva@globant.com>
2020-04-12 11:18:37 -0300

    added a cities array for easier handleing

---
+++ src/app/core/services/cities.ts
@@ -0,0 +1,83 @@
+export const cities =[
+    {
+        "id": 3433955,
+        "name": "Ciudad Autónoma de Buenos Aires",
+        "country": "AR",
+        "coord": {
+          "lon": -58.450001,
+          "lat": -34.599998
+        }
+      },
+      {
+        "id": 1819729,
+        "name": "Hong Kong",
+        "country": "HK",
+        "coord": {
+          "lon": 114.157692,
+          "lat": 22.285521
+        }
+      },
+      {
+        "id": 5368361,
+        "name": "Los Angeles",
+        "country": "US",
+        "coord": {
+          "lon": -118.243683,
+          "lat": 34.052231
+        }
+      },
+      {
+        "id": 4164138,
+        "name": "Miami",
+        "country": "US",
+        "coord": {
+          "lon": -80.193657,
+          "lat": 25.774269
+        }
+      },
+      {
+        "id": 5419384,
+        "name": "Denver",
+        "country": "US",
+        "coord": {
+          "lon": -104.984703,
+          "lat": 39.739151
+        }
+      },
+      {
+        "id": 3652462,
+        "name": "Quito",
+        "country": "EC",
+        "coord": {
+          "lon": -78.524948,
+          "lat": -0.22985
+        }
+      },
+      {
+        "id": 1850207,
+        "name": "Tokio",
+        "country": "JP",
+        "coord": {
+          "lon": 137.183334,
+          "lat": 35.349998
+        }
+      },
+      {
+        "id": 2968815,
+        "name": "Paris",
+        "country": "FR",
+        "coord": {
+          "lon": 2.3486,
+          "lat": 48.853401
+        }
+      },
+      {
+        "id": 6359304,
+        "name": "Madrid",
+        "country": "ES",
+        "coord": {
+          "lon": -3.68275,
+          "lat": 40.489349
+        }
+      },
+]
