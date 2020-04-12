fe349d9a13b4173724a5fc72eb5ba393d3396c5b
pablo <pabloariel.silva@globant.com>
2020-04-12 00:23:17 -0300

    simulates backend appointment creation

--- src/app/core/services/appointment.service.ts
+++ src/app/core/services/appointment.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { IAppointment } from '@home/home.component';
+import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AppointmentService {
+  private appointmentNmbr;
+  constructor() {}
 
-  constructor() { }
+  //should be replaced with api post, second param is added to simulate backend response
+  createAppointment(appointment: IAppointment, id: number): Observable<IAppointment> {
+    appointment.id = id;
+    return of(appointment);
+  }
 }
