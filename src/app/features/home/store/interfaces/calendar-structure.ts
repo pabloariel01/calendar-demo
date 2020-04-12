f5801eee8ac9b3ea03fe8f88c4d10463cc2ee374
pablo <pabloariel.silva@globant.com>
2020-04-11 22:21:10 -0300

    calendar data structure refactored

--- src/app/features/home/store/interfaces/calendar-structure.ts
+++ src/app/features/home/store/interfaces/calendar-structure.ts
@@ -1,29 +1,28 @@
 import * as moment from 'moment';
 import { IAppointment } from '../../home.component';
+import { IDictionary } from '@core/utils/interfaces/dictionary';
 
 export interface ICalendarState {
+  years?: IDictionary<IMonth>;
   selectedDay: moment.Moment;
-  years?: IYear[];
+  appointmentNmbr: number;
 }
-
-interface IYear {
-  id: number;
-  months?: IMonth[] | [];
-}
+//changed in order to look like a rest api respose and easier to find like 2020.18.3
 
 interface IMonth {
-  id: number;
-  appointment: IAppointment[];
+  month?: IDictionary<IDay>[];
+}
+
+interface IDay {
+  day?: IDictionary<IAppointment>[];
 }
 
 const today = moment();
 
 export const initialState: ICalendarState = {
   selectedDay: moment(),
-  years: [
-    {
-      id: +moment(today).format('YYYY'),
-      months: []
-    }
-  ]
+  years: {
+    [moment(today).format('YYYY')]: {}
+  },
+  appointmentNmbr: 0
 };
