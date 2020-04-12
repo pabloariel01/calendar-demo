e52fed8919f727f5083b0c21723a2c701a18415a
pablo <pabloariel.silva@globant.com>
2020-04-12 00:24:26 -0300

    renamed component

--- src/app/shared/components/calendar/day-cell/day-cell.component.ts
+++ src/app/shared/components/calendar/day-cell/day-cell.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { IAppointment } from '@home/home.component';
 
 @Component({
   selector: 'app-day-cell',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./day-cell.component.scss']
 })
 export class DayCellComponent implements OnInit {
+  @Input() day: IAppointment;
+  @Output() eraseAll: EventEmitter<IAppointment> = new EventEmitter();
+  @Output() addAppointment: EventEmitter<IAppointment> = new EventEmitter();
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
+  ngOnInit() {}
+  public NewAppointment(event): void {
+    event.preventDefault();
+    console.log(event);
+    // this.addAppointment.emit()
   }
 
+  public removeAllAppointment(event): void {
+    event.preventDefault();
+    // this.eraseAll.emit()
+  }
 }
